17e6116c2917bed1911085c820a65c7b4fdea0ec
noga1103 <117585750+noga1103@users.noreply.github.com>
2024-06-10 09:04:30 +0300

    Update EqT_utils.py

--- EQTransformer/core/EqT_utils.py
+++ EQTransformer/core/EqT_utils.py
@@ -77,7 +77,7 @@ class MambaSSM(Layer):
 
     def call(self, x, delta, B, C, D):
         # Compute the hidden states using the Mamba SSM equations
-h = ag__.converted_call(ag__.ld(tf).scan, (ag__.autograph_artifact(lambda h_prev, x_t: ag__.converted_call(ag__.ld(tf).matmul, (ag__.ld(x_t), ag__.converted_call(ag__.ld(tf).transpose, (ag__.ld(B), [0, 2, 1]), None)), None) + ag__.converted_call(ag__.ld(tf).matmul, (ag__.ld(h_prev), ag__.converted_call(ag__.ld(tf).exp, (ag__.ld(delta) * ag__.ld(self).A,), None)), None)), ag__.ld(x)), dict(initializer=ag__.converted_call(ag__.ld(tf).zeros, ((ag__.converted_call(ag__.ld(tf).shape, (ag__.ld(x),), None)[0], ag__.converted_call(ag__.ld(tf).shape, (ag__.ld(x),), None)[-1]),), None)), None)
+        h = ag__.converted_call(ag__.ld(tf).scan, (ag__.autograph_artifact(lambda h_prev, x_t: ag__.converted_call(ag__.ld(tf).matmul, (ag__.ld(x_t), ag__.converted_call(ag__.ld(tf).transpose, (ag__.ld(B), [0, 2, 1]), None)), None) + ag__.converted_call(ag__.ld(tf).matmul, (ag__.ld(h_prev), ag__.converted_call(ag__.ld(tf).exp, (ag__.ld(delta) * ag__.ld(self).A,), None)), None)), ag__.ld(x)), dict(initializer=ag__.converted_call(ag__.ld(tf).zeros, ((ag__.converted_call(ag__.ld(tf).shape, (ag__.ld(x),), None)[0], ag__.converted_call(ag__.ld(tf).shape, (ag__.ld(x),), None)[-1]),), None)), None)
         # Compute the output
         y = tf.matmul(h, C) + tf.matmul(x, D)
 
