68388e4601e4ae8acb99d6fe7265b68a605dce05
noga1103 <117585750+noga1103@users.noreply.github.com>
2024-06-14 14:26:33 +0300

    Update __init__.py

--- EQTransformer/core/__init__.py
+++ EQTransformer/core/__init__.py
@@ -1,7 +1,7 @@
 
 name='core'
-
-from .EqT_utils import *
+from .EqT_utils_transformer import *
+from .EqT_utils_mamba import *
 from .trainer import trainer
 from .tester import tester
 from .predictor import predictor
